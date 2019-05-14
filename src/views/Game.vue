be755654f4aa62d57d19a402a2597e89c1a35466
wakaolian <last62us@gmail.com>
2019-05-14 11:16:22 +0900

    janken-app

---
+++ src/views/Game.vue
@@ -0,0 +1,14 @@
+<template>
+    <Janken></Janken>
+</template>
+
+<script>
+import Janken from '@/components/Janken'
+export default {
+    name : 'Game',
+    components: {
+        Janken
+    }
+}
+</script>
+
